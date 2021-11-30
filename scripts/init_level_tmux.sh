950bbb37ebde6cbf8fc854299c57950c5f2c4944
Ariel Camperi <ariel.camperi@level.com>
2021-11-29 22:25:03 -0500

    fix level tmux init script for when tmux is not running

--- scripts/init_level_tmux.sh
+++ scripts/init_level_tmux.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-session_exists=$(tmux list-sessions | grep 'dev')
-if [ "$session_exists" = "" ]
-then
+session_exists=0
+tmux has-session -t dev 2>/dev/null || session_exists=$?
+if [ $session_exists != 0 ]; then
     tmux new-session -d -s 'dev' -n 'level' -c ~/code/level
     tmux new-window -n 'level-nvim' -c ~/code/level nvim .
     tmux new-window -n 'dotfiles' -c ~/dotfiles
