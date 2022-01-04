cb91c4d3a89edbdc7bb1918c505ffc6a0a7be7f7
Ariel Camperi <ariel.camperi@gmail.com>
2022-01-04 11:55:40 -0600

    fix nvim config symlink in install script

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ ln -nfsv "$dotfiles_dir/vim/vimrc" ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 mkdir -p ~/.config/nvim
-ln -nfsv "$dotfiles_dir/nvim/init.vim" ~/.config/nvim/init.nvim
+ln -nfsv "$dotfiles_dir/nvim/init.vim" ~/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
 
