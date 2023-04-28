8f3cd95cccaa0f440b6b7e143e54c40e330231a2
Ariel Camperi <ariel.camperi@level.com>
2023-04-28 16:08:08 -0700

    add shortcut for fzf buffer lines

--- nvim/init.lua
+++ nvim/init.lua
@@ -296,6 +296,7 @@ vim.opt.runtimepath:append('/usr/local/opt/fzf')
 vim.keymap.set('n', '<leader>o', function() vim.cmd([[:GFiles --exclude-standard --cached --others]]) end)
 vim.keymap.set('n', '<leader>O', function() vim.cmd.Files() end)
 vim.keymap.set('n', '<leader>/', function() vim.cmd.Rg() end)
+vim.keymap.set('n', '<leader>l', function() vim.cmd.BLines() end)
 vim.api.nvim_set_var('fzf_action', {
     ['ctrl-t'] = 'tab split',
     ['ctrl-s'] = 'split',
