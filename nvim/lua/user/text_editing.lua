98b97938596273a21a7f6dcb2e82f55623f1c434
Ariel Camperi <ariel.camperi@level.com>
2023-05-15 19:28:13 -0700

    fix highlight on yank

--- nvim/lua/user/text_editing.lua
+++ nvim/lua/user/text_editing.lua
@@ -81,7 +81,13 @@ local highlight_yank_augroup = vim.api.nvim_create_augroup('highlight_yank', { c
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = highlight_yank_augroup,
     pattern = '*',
-    callback = function() vim.highlight.on_yank() end,
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'Visual',
+            timeout = 500,
+            on_visual = false,
+        })
+    end,
 })
 
 -- buffer
